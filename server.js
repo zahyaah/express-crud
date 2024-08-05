dfd29550ee63ac025c928e354356daeb5c1fde95
zahyaah <itszayd2003@gmail.com>
2024-08-05 18:57:54 +0530

    clean

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express(); 
 const PORT = process.env.PORT; 
 
-const shoes = []; 
-const id = 1; 
 
 const exampleShoes = [
     { "id": 1, "shoe": "Nike Vaporfly 3 Electric", "price": 21000},
