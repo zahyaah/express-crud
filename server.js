17841985a3f5d7dc6b58f843c43e3df80f14db53
zahyaah <itszayd2003@gmail.com>
2024-08-05 18:04:19 +0530

    add-id, update

--- server.js
+++ server.js
@@ -24,6 +24,38 @@ app.get("/shoes", (req, res) => {
     res.status(200).send(exampleShoes);
 })
 
+app.get("/shoes/:id", (req, res) => {
+    let id = parseInt(req.params.id); 
+    const found = exampleShoes.find(shoe => shoe.id === id);
+
+    if (!found) 
+        return res.status(404).send({'Error': `Shoes with ID ${id} not found!`});
+
+    res.status(200).send(found);
+})
+
+app.post("/shoes", (req, res) => {
+    let newId = exampleShoes.length + 1; 
+    const {shoe, price} = req.body; 
+
+    exampleShoes.push({"id": newId, shoe, price});
+    res.status(204).send("Shoe Added");
+})
+
+app.put("/shoes/:id", (req, res) => {
+    let getId = parseInt(req.params.id);
+    const found = exampleShoes.find(shoe => shoe.id === getId);
+
+    if (!found)
+        return res.status(404).send({"Error" : `Shoe with ID ${getId} not found!`})
+
+    const {shoe, price} = req.body; 
+    found.shoe = shoe; 
+    found.price = price; 
+
+    res.status(204).send(`Shoe with ID ${getId} updated!`);
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 })
